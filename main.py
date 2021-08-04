6c9f07792f27e5e4fffed954ecf6a898ce5a1d9b
Aidan <aidan.fu000@gmail.com>
2021-08-04 16:29:28 -0400

    comment every function and remove needless nested loop

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import numpy as np
-import math
+
+"""
+This project was inspired by and utilizes much of the info described in https://machinelearningmastery.com/naive-bayes-classifier-scratch-python/
+However the code differs as this aims to implement naive bayes using numpy.
+"""
 
 
 def load_data(path, delimiter, usecols, skiprows):
@@ -33,7 +37,7 @@ def load_data(path, delimiter, usecols, skiprows):
 
 
 def separate_by_class(X, Y):
-    """
+    """ Seperate the input data by class.
 
     Parameters
     ----------
@@ -63,11 +67,12 @@ def separate_by_class(X, Y):
         temp = temp[..., np.newaxis]
         # take the initial array of zeros we created and append the attributes to its columns.
         separated[Y[0, i]] = np.append(separated[Y[0, i]], temp, axis=1)
+
     return separated
 
 
 def summarize_data(X, Y):
-    """
+    """ Summarize the data into 3 key values, the mean, standard deviation and the number of data samples.
 
     Parameters
     ----------
@@ -79,20 +84,18 @@ def summarize_data(X, Y):
     Returns
     -------
     summaries: dict
-        Dictionary of tuple for each label. First item is the means of the label's data sample's attributes.
-        The second item is the standard deviations of the label's data sample's attributes.
-        The third item is the number of attributes that all data samples contain.
+        Dictionary with key as the label, and value as a tuple of 3 items.
 
     """
     separated_data = separate_by_class(X, Y)
     summaries = {}
 
     for label, input in separated_data.items():
-        # input is an ndarray where each column is a data sample and each row is an attribute
+        # input is an ndarray where each column is a data sample and each row is an input variable
 
-        # Ends up as a single row containing the mean of each data sample's attributes.
+        # Ends up as a single column where each row is the mean of that input variable.
         means = np.mean(input, axis=1, keepdims=True)
-        # Ends up as a single row containing the standard deviation of each data sample's attributes.
+        # Ends up as a single column where each row is the standard deviation of that input variable.
         stds = np.std(input, axis=1, keepdims=True)
         summaries[label] = (means, stds, input.shape[1])
 
@@ -100,35 +103,135 @@ def summarize_data(X, Y):
 
 
 def gaussian_prob_density_func(X, means, stds):
+    """ Calculates a gaussian probability density function given the input, means, and standard deviations.
+    It maps the input X onto the normal distribution and returns those mapped values.
+
+    Parameters
+    ----------
+    X:ndarray
+        input data.
+    means:ndarray
+        the means of the attributes of the input data.
+    stds:ndarray
+        the standard deviations of the attributes of the input data.
+
+    Returns
+    -------
+    An ndarray containing each input value mapped onto the normal distribution.
+
+    """
     exponent = np.exp(-np.square((X - means)) / (2 * np.square(stds)))
-    return (1 / (np.sqrt(2 * np.pi) * stds)) * exponent
+    val = (1 / (np.sqrt(2 * np.pi) * stds)) * exponent
+    return val
 
 
-def calculate_class_probabilities(summaries, X):
+def calculate_class_probabilities(class_summaries, X):
+    """ Probabilities are calculated using bayes theorem P(class|data) = (P(data|class) * P(class)) / P(data).
+    In our case we don't need the exact probability we just want a value that is still maximized
+    meaning we still take the highest value to be the true class. This allows us to avoid integrating
+    between two points on the normal distribution, and dividing by the P(data) which stops needless computations.
+
+    In this naive bayes implementation the probability is calculated through the formula
+    P(class=0|X1,X2) = P(X1|class=0) * P(X2|class=0) * P(class=0) where each input variable's probability
+    of being in that class is being multiplied together which treats it independently hence naive.
+
+
+    Parameters
+    ----------
+    class_summaries:tuple
+        The summaries of the training data
+    X:ndarray
+        The input data that will be classified
+
+    Returns
+    -------
+    Dictionary where each key is a label and the value is an array where for every index there is
+    the total "probability" of that label being true for the input relating to that index.
+    Eg. index 0 contains the total "probability" of that label being true for the first set of input variables.
+
+    """
     class_probabilities = {}
     total_data_count = 0
 
-    for label, summary in summaries.items():
-        total_data_count += summary[2]
+    for label, class_summary in class_summaries.items():
+        total_data_count += class_summary[2]
 
-    for _, _ in summaries.items():  # loop through each item
-        for label, summary in summaries.items():  # calculate the probability of each column in 'input' being of label 'lbl'
-            _, _, num_labeled = summary
-            if label not in class_probabilities:
-                class_probabilities[label] = math.log(num_labeled / total_data_count, 10)  # log probabilities.
-            means, stds, _ = summary
-            class_probabilities[label] *= np.log10(np.prod(gaussian_prob_density_func(X, means, stds), axis=0))
+    # check for each class/label
+    for label, class_summary in class_summaries.items():
+        _, _, data_count = class_summary
+        if label not in class_probabilities:
+            # probability that the given class is the correct one (P(class))
+            class_probabilities[label] = data_count / total_data_count
+        means, stds, _ = class_summary
+        # probabilities that each input variable belongs to the given class/label are calculated using the
+        # prob density function of that class_summary.
+        # Each input variable probability is multiplied together
+        class_probabilities[label] *= np.prod(gaussian_prob_density_func(X, means, stds), axis=0)
 
     return class_probabilities
 
 
-if __name__ == '__main__':
-    X_summary, Y_summary = load_data('IrisTrainingBinary.csv', ',', (1, 2, 3, 4, 5), 1)
-    summaries = summarize_data(X_summary, Y_summary)
-    X, Y = load_data('IrisTestBinary.csv', ',', (1, 2, 3, 4, 5), 1)
+def testModel():
+    # load the training data
+    X_loaded, Y_loaded = load_data('csv_data/IrisTraining.csv', ',', (1, 2, 3, 4, 5), 1)
+
+    # summarize the training data
+    class_summaries = summarize_data(X_loaded, Y_loaded)
+
+    # load the test data
+    X, Y = load_data('csv_data/IrisTest.csv', ',', (1, 2, 3, 4, 5), 1)
+
+    # calculate the probabilities using the normal distribution obtained from the training data.
+    class_probabilities = calculate_class_probabilities(class_summaries, X)
+
+    print(class_probabilities)
+
+    # loop through all inputs
+    for i in range(X.shape[1]):
+        # take the highest probability
+        label = max(class_probabilities[0][i], class_probabilities[1][i], class_probabilities[2][i])
+
+        # check if true label is same as predicted label
+        if label == class_probabilities[0][i]:
+            label = 0
+            print(label == Y[0][i])
+        if label == class_probabilities[1][i]:
+            label = 1
+            print(label == Y[0][i])
+        if label == class_probabilities[2][i]:
+            label = 2
+            print(label == Y[0][i])
+
+
+def testModelBinary():
+    # load the training data
+    X_loaded, Y_loaded = load_data('csv_data/IrisTrainingBinary.csv', ',', (1, 2, 3, 4, 5), 1)
+
+    # summarize the training data
+    summaries = summarize_data(X_loaded, Y_loaded)
+
+    # load the test data
+    X, Y = load_data('csv_data/IrisTestBinary.csv', ',', (1, 2, 3, 4, 5), 1)
+
+    # calculate the probabilities using the normal distribution obtained from the training data.
     class_probabilities = calculate_class_probabilities(summaries, X)
 
     print(class_probabilities)
 
-    if class_probabilities[0][0] > class_probabilities[1][0]:
-        print('correct')
+    # loop through all inputs
+    for i in range(X.shape[1]):
+        # take the highest probability
+        label = max(class_probabilities[0][i], class_probabilities[1][i])
+
+        # check if true label is same as predicted label
+        if label == class_probabilities[0][i]:
+            label = 0
+            print(label == Y[0][i])
+        if label == class_probabilities[1][i]:
+            label = 1
+            print(label == Y[0][i])
+
+
+if __name__ == '__main__':
+    testModel()
+
